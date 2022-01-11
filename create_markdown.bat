62ccbee37b065ef1a2df9928dc30baf87322a0d9
Spiers Alex <advspiers@gmail.com>
2022-01-11 15:32:09 +0000

    Add Affinity Steroid stability

--- create_markdown.bat
+++ create_markdown.bat
@@ -1,4 +1,4 @@
 @echo off
 PATH "C:\Program Files\R\R-4.0.5\bin\x64";%path%
 @echo on
-Rscript --vanilla create_report.R --input="produce_tables.Rmd" --filename="output_tables.html" --dir="results"
+Rscript --vanilla create_report.R --input="produce_tables_urine.Rmd" --filename="urine_output_tables.html" --dir="results"
--- create_report.R
+++ create_report.R
@@ -3,12 +3,12 @@ args = R.utils::commandArgs(trailingOnly=TRUE, asValues=TRUE)
 print(args)
 
 if (is.null(args$input)){
-    input = './run_tms_steroid_stability.Rmd'
+    input = './run_affinity_steroid_stability.Rmd'
 } else {
     input = args$input
 }
 if (is.null(args$filename)){
-    filename = "tms_steroid_fitting.html"
+    filename = "affinity_steroid_fitting.html"
 } else {
     filename = args$filename
 }
